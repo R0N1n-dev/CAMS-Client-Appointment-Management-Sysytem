933af53bce9b7a962d420439722242c3af4c5458
Kalungi Moses <mdrewj@live.com>
2023-09-27 09:07:06 +0300

    Fixed Schema relation issue

--- server/api/appointments/[id].js
+++ server/api/appointments/[id].js
@@ -1,5 +1,23 @@
 import prisma from "~~/lib/prisma.js";
 export default defineEventHandler(async (event) => {
+  if ((event.node, req.method === "PUT")) {
+    const body = await readBody(event);
+    const updatedAppointment = await prisma.appointment.update({
+      where: {
+        id: event.context.params.id,
+      },
+      data: {
+        results: {
+          create: {
+            feedback: body.feedback,
+          },
+        },
+      },
+    });
+    console.log(updatedAppointment);
+    return { success: true };
+  }
+
   const specificAppointment = await prisma.appointment.findUnique({
     where: {
       id: event.context.params.id,
